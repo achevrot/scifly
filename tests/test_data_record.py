63bcfad98bfa8f2a4c705138e2787611f103c8b7
Wirden <antoine.chevrot@gmail.com>
2020-12-04 13:29:01 +0100

    adding first unit test to try the package

---
+++ tests/test_data_record.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 11:51:07 2020
+
+@author: antoine
+"""
+import tensorflow as tf
+from data_record import DataRecord
+
+
+sequence_features = {
+            'alt': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+            'd_a': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+            'd_b': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+            'd_c': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+            'd_d': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+            'delta': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+            'hdg_delta': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+            'label': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+            'lat': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+            'lon': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+            'spd': tf.io.FixedLenSequenceFeature([], dtype=tf.float32),
+        }
+
+
+
+def test_getters():
+    record = DataRecord("tests/test_data/WZZ1A_test.tfrecord", sequence_features)
+    assert isinstance(record.get_icaos(), list)
