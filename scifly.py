8a05a5b3c132641bb5ae8b043c9225abee319fec
Wirden <antoine.chevrot@gmail.com>
2020-12-04 11:42:30 +0100

    adding scifly with data cleaning method

---
+++ scifly.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  1 15:03:35 2020
+
+@author: antoine
+"""
+
+import pandas as pd
+from vincenty import vincenty
+from datetime import timedelta
+
+def data_cleaner(input_df:pd.DataFrame):
+    
+    df = input_df.copy()
+    
+    df['lat_shift_1'] = df['latitude'].shift(-1)
+    df['lon_shift_1'] = df['longitude'].shift(-1)
+    df['lat_shift_30'] = df['latitude'].shift(300)
+    df['lon_shift_30'] = df['longitude'].shift(300)
+    
+    df["delta_close"] = df.apply(
+        lambda x: vincenty((x['latitude'], x['longitude']),
+                           (x['lat_shift_1'], x['lon_shift_1'])), axis = 1)
+    df["delta_far"] = df.apply(
+        lambda x: vincenty((x['latitude'], x['longitude']),
+                           (x['lat_shift_30'], x['lon_shift_30'])), axis = 1) 
+    
+    r_c = df['delta_close'].rolling(timedelta(seconds=300))
+    r_f = df['delta_far'].rolling(timedelta(seconds=300))
+    
+    result = df[((df.delta_close - r_c.median()).abs() < 3 * r_c.std())
+                & ((df.delta_far - r_f.median()).abs() < 3 * r_f.std())]
+
+    return result
