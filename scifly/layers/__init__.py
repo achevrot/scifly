73ae1c529532b9b4bc5332f9d68077ba4356543a
Wirden <antoine.chevrot@gmail.com>
2021-01-25 16:17:15 +0100

    changing imports in init

--- scifly/layers/__init__.py
+++ scifly/layers/__init__.py
@@ -1,4 +1,4 @@
 from scifly.layers.concatenate import Concatenate
 from scifly.layers.decoder import Decoder
 from scifly.layers.encoder import Encoder
-from scifly.layers.phase_splitter import Phase_splitter
+from scifly.layers.phase_splitter import PhaseSplitter
