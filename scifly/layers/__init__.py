04d589d7cafa5f82ed965557550cce962e9580ed
Wirden <antoine.chevrot@gmail.com>
2021-01-25 16:05:48 +0100

    Ditto layer module

---
+++ scifly/layers/__init__.py
@@ -0,0 +1,4 @@
+from scifly.layers.concatenate import Concatenate
+from scifly.layers.decoder import Decoder
+from scifly.layers.encoder import Encoder
+from scifly.layers.phase_splitter import Phase_splitter
