e3e15dc5dd59e92e21315393fc57898f86286778
Wirden <antoine.chevrot@gmail.com>
2021-01-25 16:41:41 +0100

    changing tf_concat

--- scifly/layers/concatenate.py
+++ scifly/layers/concatenate.py
@@ -1,15 +1,12 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Concatenate as tf_concat
 
 class Concatenate(tf.keras.layers.Layer):
     
-    def __init__(self, name="concatenate", **kwargs):
-        super(Concatenate, self).__init__(name=name, **kwargs)
-        self.concat = tf_concat()
-    
     def call(self, inputs):
         asc_dec, cru_dec, des_dec, concat_index = inputs
-        concat_dec = self.concat([asc_dec, cru_dec, des_dec], 0)
+        concat_dec = tf.keras.layers.concatenate([asc_dec,
+                                                  cru_dec,
+                                                  des_dec], 0)
         return tf.gather(concat_dec, tf.argsort(concat_index), axis=0)
 
     def get_config(self):
